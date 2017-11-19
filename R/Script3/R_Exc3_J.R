2550994129c912f574ddc83df094fb4dd94e664e
jcantoral <31830452+jcantoral@users.noreply.github.com>
2017-11-19 13:22:48 +0100

    Update R_Exc3_J.R
    All excercises completed. Some style details pending, such us uniform spacing.

--- R/Script3/R_Exc3_J.R
+++ R/Script3/R_Exc3_J.R
@@ -12,7 +12,7 @@ table(sex)
 table(sex,leuk.class)
 
 xtabs(~sex+leuk.class)
-mytable <- as.data.frame(xtabs(~sex+leuk.class))
+mytable <- as.data.frame(xtabs (~sex + leuk.class))
 
 #Section 2: Two subsetting operations
 pval <- apply(leuk.dat.m, 1,
@@ -20,7 +20,7 @@ pval <- apply(leuk.dat.m, 1,
 
 #Mean of expression of genes whose p-values < 0.01
 ## for patient 3:
-mean(leuk.dat.m[which(pval<0.01),3])
+mean(leuk.dat.m[which (pval<0.01), 3])
 
 #Median of gene 2 expression in males:
 median(leuk.dat.m[2,which(sex=="Male")])
@@ -30,3 +30,33 @@ median(leuk.dat.m[2,which(sex=="Male")])
 leuk.dat.t <- t(leuk.dat.m)
 dim(leuk.dat.m);dim(leuk.dat.t) #To see how dimensions have changed
 leuk.dat.t[1:5, 1:6]
+
+#3.1 aggregate: The median of three genes by condition
+aggregate(leuk.dat.t [,c(1,2124,2600)] ~ leuk.class, FUN = median)
+
+#3.2 aggregate: The median of three genes by condition and sex
+aggregate(x = leuk.dat.t [,c(1,2124,2600)],
+          by = list (type = leuk.class, sex = sex),
+          FUN=median)
+
+#3.3 aggregate: The median of all the genes by condition and sex
+all.median <- aggregate( x = leuk.dat.t,
+                         by = list (type = leuk.class, sex = sex),
+                         FUN = median)
+all.median[,c(1:10)]
+dim(all.median)
+
+#3.4 aggregate: The mean and standard deviations of three genes by condition
+##and sex
+aggregate(x=leuk.dat.t [,c(1,2124,2600)],
+          by = list (type = leuk.class, sex = sex), 
+          function(x) c(mean=mean(x),sd=sd(x)))
+
+#3.5 by and aggregate: those three genes again, but now use “summary”
+by (data = leuk.dat.t [,c(1,2124,2600)],
+    INDICES = list (type = leuk.class, sex = sex),
+    FUN = summary ) #Argument names are not required, but informative.
+
+aggregate(x=leuk.dat.t [,c(1,2124,2600)],
+          by = list (type = leuk.class, sex = sex), 
+          function(x) summary(x))
