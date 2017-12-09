61477d19ba6463afd7d81e8c65a1f216a7536679
jcantoral <31830452+jcantoral@users.noreply.github.com>
2017-12-09 20:11:39 +0100

    Update R_exc4_J.R
    Final draft. Cleaned code and using self-defined functions.

--- R/Script4/R_exc4_J.R
+++ R/Script4/R_exc4_J.R
@@ -6,47 +6,25 @@ rm(list=ls())
 genes <- sapply(list.files(),
                 function(x) scan(x, what=""))
 
-#genes.lengths <- sapply(genes, nchar)
-
 stopCodons <- c("TAA", "TAG", "TGA")
 
-with.stop <- grep(paste(stopCodons, collapse="|")
-                  , genes
-                  , value = TRUE)
+codon.search <- function(codons,seqs) {
+    grep(paste(codons, collapse="|")
+         , seqs
+       , value = TRUE)
+}
 # grep understands the pipe symbol "|" as an OR operator
 
+with.stop <- codon.search(stopCodons, genes)
 without.stop <- setdiff(genes, with.stop)
-
-#----
-#Some stats for my curiosity only
-boxplot(sapply(with.stop, nchar)
-        ,sapply(without.stop, nchar)
-        )
 
 summary(sapply(with.stop, nchar))
 summary(sapply(without.stop, nchar))
 
-#----
+T.content <- function(seq) {
+    seq.Ts <- gsub("[^T]","",seq)
+    return(sapply(seq.Ts, nchar)/ sapply(seq, nchar))
+    }
 
-with.stop.Ts <-gsub("[^T]","",with.stop)
-without.stop.Ts <- gsub("[^T]","",without.stop)
-
-with.stop.Tcontent <- sapply(with.stop.Ts, nchar) / 
-  sapply(with.stop, nchar)
-without.stop.Tcontent <- sapply(without.stop.Ts, nchar) / 
-  sapply(without.stop, nchar)
-
-#boxplot(with.stop.Tcontent, without.stop.Tcontent)
-
-summary(with.stop.Tcontent)
-summary(without.stop.Tcontent)
-
-# ----
-# I just wanted to check the distribution of T content among
-# all the sequences.
-
-genes.Ts <-gsub("[^T]","",genes)
-genes.Tcontent <- sapply(genes.Ts, nchar) / 
-  sapply(genes, nchar)
-plot(genes.lengths~genes.Tcontent)
-summary(genes.Tcontent)
+summary(T.content(with.stop))
+summary(T.content(without.stop))
